cddd229e1e01f81ea227d0d2cf3543e909f64233
cesine <cesine@yahoo.com>
2014-09-30 19:43:30 -0400

    fixes https://github.com/OpenSourceFieldlinguistics/FieldDB/issues/1652
    added sikuli test for loading sample data

---
+++ it_should_load_sample_data.py
@@ -0,0 +1,11 @@
+find("georgian_together_icon.png")
+click("georgian_together_icon.png")
+click("georgian_together_icon.png")
+
+wait(5)
+
+find(Pattern("georgian_together_sample_data.png").targetOffset(41,-38))
+click(Pattern("georgian_together_sample_data.png").targetOffset(-107,-6))
+
+find("back_button.png")
+click("back_button.png")
